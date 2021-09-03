315bbfc453ed31011f7d43c447cee8eef505c0ba
Parth Bhardwaj <parthb.git@gmail.com>
2021-09-03 17:12:14 +0530

    testing aos on social links

--- _site/scripts/links.js
+++
@@ -1,5 +0,0 @@
-const linksContainer = document.querySelector(".social-container");
-setTimeout(() => {
-  linksContainer.style.opacity = 1;
-  linksContainer.style.transform = "scale(1)";
-}, 400);
