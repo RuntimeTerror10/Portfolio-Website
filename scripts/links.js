35795d29186cc98d3b2cff23f8ff9628462cf830
Parth Bhardwaj <parthb.git@gmail.com>
2021-09-03 13:53:50 +0530

    increased duration of setTimeout by 100ms

--- scripts/links.js
+++ scripts/links.js
@@ -2,4 +2,4 @@ const linksContainer = document.querySelector(".social-container");
 setTimeout(() => {
   linksContainer.style.opacity = 1;
   linksContainer.style.transform = "scale(1)";
-}, 300);
+}, 400);
