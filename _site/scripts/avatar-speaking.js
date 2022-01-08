8f6a8caa8d917ea3154953eda1239f3b4220557b
RuntimeTerror10 <parthb.git@gmail.com>
2022-01-09 02:19:25 +0530

    minor changes and media queries

--- _site/scripts/avatar-speaking.js
+++ _site/scripts/avatar-speaking.js
@@ -1,8 +1,6 @@
 const idle = document.querySelector("#art3");
 const speak1 = document.querySelector("#art1");
 const speak2 = document.querySelector("#art2");
-const btn = document.querySelector(".btn");
-const homeBtn = document.querySelector(".nav-link");
 
 speak2.classList.remove("speaking-animation");
 speak2.classList.add("speaking-animation");
@@ -12,5 +10,4 @@ setTimeout(function () {
 speak2.addEventListener("animationend", () => {
   speak2.classList.remove("speaking-animation");
   idle.style.zIndex = "3";
-  btn.style.opacity = "1";
 });
