c9df701f8d4a5c7ce96a114fe1bdb5c455062e3f
RuntimeTerror10 <parthb.git@gmail.com>
2022-01-09 01:58:18 +0530

    replace header-layout.njk with page-layout for index page

--- scripts/avatar-speaking.js
+++ scripts/avatar-speaking.js
@@ -2,9 +2,13 @@ const idle = document.querySelector("#art3");
 const speak1 = document.querySelector("#art1");
 const speak2 = document.querySelector("#art2");
 const btn = document.querySelector(".btn");
+const homeBtn = document.querySelector(".nav-link");
+
+speak2.classList.remove("speaking-animation");
+speak2.classList.add("speaking-animation");
 setTimeout(function () {
   idle.style.zIndex = -3;
-}, 1000);
+}, 500);
 speak2.addEventListener("animationend", () => {
   speak2.classList.remove("speaking-animation");
   idle.style.zIndex = "3";
