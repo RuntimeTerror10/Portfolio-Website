c260c9b8d9cf96ca90bce0d3aee3103a41fd8187
RuntimeTerror10 <parthb.git@gmail.com>
2022-03-14 11:20:11 +0530

    styling changes and removed extra pages

--- _data/nav.js
+++ _data/nav.js
@@ -2,6 +2,5 @@ module.exports = [
   { label: "Home", url: "/" },
   { label: "Projects", url: "/projects" },
   { label: "Blog", url: "/blog" },
-  { label: "Contact", url: "/contact" },
   { label: "About", url: "/me" },
 ];
